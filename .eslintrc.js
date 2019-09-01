2560046c0407769928b78fae6ebc7096f40a6410
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2019-09-01 16:21:49 +0900

    fix by lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'camelcase': 'off',
-    'prettier/prettier': 'error'
+    'camelcase': 'off'
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
