d59a0cede66cab526343897d33d2ec1903d5738e
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2019-09-03 00:36:14 +0900

    implement runtime cache

---
+++ docs/precache-manifest.a9ce13d65ea87a0299d46ca1f0b18812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-sample-pwa/robots.txt"
+  },
+  {
+    "revision": "c6ca92da8004446b4bec",
+    "url": "/vue-sample-pwa/js/chunk-vendors.f6ef7d56.js"
+  },
+  {
+    "revision": "4515ba1d90b8f472a9b7",
+    "url": "/vue-sample-pwa/js/app.b35b085a.js"
+  },
+  {
+    "revision": "7d7a465db8298326f6ef",
+    "url": "/vue-sample-pwa/js/about.c0f805f1.js"
+  },
+  {
+    "revision": "148da428478c90ece3913b773838bd66",
+    "url": "/vue-sample-pwa/index.html"
+  },
+  {
+    "revision": "4515ba1d90b8f472a9b7",
+    "url": "/vue-sample-pwa/css/app.75803024.css"
+  }
+];
